f3e065144814e35957803c9adcb2f02b3a9455f8
Edmar Batista <edmarbpaiva@gmail.com>
2024-09-02 06:39:05 -0300

    Unidade 2 - Aula 1 - Funções

--- 02 Programação Orientada a Objetos/01 Funções/funções.py
+++ 02 Programação Orientada a Objetos/01 Funções/funções.py
@@ -45,3 +45,25 @@ print(calcular(5,10,'+'))
 print(calcular(5,10,'-'))
 print(calcular(5,10,'a'))
 
+# função que retorna o maior valor de uma lista
+def encontrar_maior_valor(lista):
+    maior = lista[0]
+    for valor in lista:
+        if valor > maior:
+            maior = valor
+    return maior
+
+numeros = [2, 8, 4, 10, 5]
+maior_numero = encontrar_maior_valor(numeros)
+print("Maior número",maior_numero) # saída: 10
+
+
+#  As funções em Python podem retornar múltiplos valores usando tuplas.
+def calcular_quadrado_e_cubo(numero):
+    quadrado = numero ** 2
+    cubo = numero ** 3
+    return quadrado, cubo
+
+resultado_quadrado, resultado_cubo = calcular_quadrado_e_cubo(3)
+print("resultado_quadrado",resultado_quadrado) # saída: 9
+print("resultado_cubo",resultado_cubo) # saída: 27
